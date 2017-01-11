15dec84d3896eaa2930598b8b6b936d01f900c33
feiliu3k <1104289018@qq.com>
2017-01-11 15:09:34 +0800

    dx

--- app/Jrsxremark.php
+++ app/Jrsxremark.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Jrsxremark extends Model
+class JrsxRemark extends Model
 {
     protected $table = 'jrsxremark';
     protected $primaryKey='id';
